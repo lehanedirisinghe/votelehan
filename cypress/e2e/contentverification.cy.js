de0f15e2d9226ad61fc452a73704dfff9cdc0cd6
Lehan <lehanedirisinghe@gmail.com>
2024-06-12 23:44:11 +0530

    Content edits, fixed up header / js footer

--- cypress/e2e/contentverification.cy.js
+++ cypress/e2e/contentverification.cy.js
@@ -1,12 +1,12 @@
 // cypress/e2e/content.cy.js
 describe('Content Verification', () => {
   it('should display the correct title on the home page', () => {
-    cy.visit('http://localhost:5500/index.html');
+    cy.visit('http://127.0.0.1:5500/index.html');
     cy.contains('Vote Lehan'); // Replace with the actual title
   });
 
   it('should have a specific paragraph in the about page', () => {
-    cy.visit('http://localhost:5500/policy.html');
-    cy.contains('President.'); // Replace with actual content
+    cy.visit('http://127.0.0.1:5500/policy.html');
+    cy.contains('President'); // Replace with actual content
   });
 });
--- cypress/e2e/navigationtest.cy.js
+++ cypress/e2e/navigationtest.cy.js
@@ -1,13 +1,13 @@
 // cypress/e2e/navigation.cy.js
 describe('Navigation', () => {
   it('should navigate to the home page', () => {
-    cy.visit('http://localhost:5500'); // Replace with your local server URL
+    cy.visit('http://127.0.0.1:5500'); // Replace with your local server URL
     cy.contains('VoteLehan').click();
     cy.url().should('include', '/index.html');
   });
 
   it('should navigate to the about page', () => {
-    cy.visit('http://localhost:5500/policy.html');
+    cy.visit('http://127.0.0.1:5500/policy.html');
     cy.contains('Policy').click();
     cy.url().should('include', '/policy.html');
   });
